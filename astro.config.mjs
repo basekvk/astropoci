48f47da3bf717da82d596835a7234c8c1c0c4bc2
David García <basek32@gmail.com>
2023-11-23 14:57:39 +0100

    jueves 23 noviembre mañana v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
   redirects: {
     '/old-page': '/new-page'
   }
